7c3fe3227bb6c97948ba01c81050573924966e28
ssala034 <sh.salat@gmail.com>
2023-10-29 19:58:55 -0400

    Update page and js

--- Feature.js
+++ Feature.js
@@ -1,3 +1,4 @@
+//For popup. Trouble with greying out background//
 $ = function(id) {
 	return document.getElementById(id);
 }
@@ -17,25 +18,77 @@ else if(!$('.cd-popup').hasClass('is-visible')) {
 	$('.popup-background').removeClass('visible');
 }
 
-// Trying to get popup to fade rest//
-jQuery(document).ready(function($){
-	//open popup
-	$('.cd-popup-trigger').on('click', function(event){
-		event.preventDefault();
-		$('.cd-popup').addClass('is-visible');
-	});
-	
-	//close popup
-	$('.cd-popup').on('click', function(event){
-		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
-			event.preventDefault();
-			$(this).removeClass('is-visible');
-		}
-	});
-	//close popup when clicking the esc keyboard button
-	$(document).keyup(function(event){
-    	if(event.which=='27'){
-    		$('.cd-popup').removeClass('is-visible');
-	    }
-    });
+
+function chBackcolor(clor) {
+	document.body.style.background = grey;
+ }
+
+//For Contact Form//
+const form = document.querySelector("form");
+const error = document.querySelectorAll("span.error");
+const dialog = document.querySelector(".dialog");
+
+console.log(error);
+form.addEventListener("submit", (e) => {
+    //stop the browser loading
+    e.preventDefault();
+
+    let nameValue = form.name.value;
+    let phoneValue = form.phone.value;
+    let emailValue = form.email.value;
+    let isEmpty = (nameValue || phoneValue || emailValue) === '';
+    
+	if (!isEmpty) {
+        validateName(nameValue);
+        validatePhone(phoneValue);
+        validdateMail(emailValue);
+
+        if (validateName(nameValue) && validatePhone(phoneValue) && validdateMail(emailValue))
+            location.href = "./welcome.html";
+    } else {
+
+        dialog.style.display = "block";
+    }
+
 });
+
+
+//name validator
+function validateName(name) {
+    if (/^[a-zA-Z\-]+$/.test(name)) {
+        displayError("#name_error", "none");
+        return true;
+    } else {
+        displayError("#name_error", "block");
+        return false;
+    }
+}
+
+//phone number validator
+function validatePhone(phone) {
+    if (/^\d{10}$/.test(phone)) {
+        displayError("#phone_error", "none");
+        return true;
+    } else {
+
+        displayError("#phone_error", "block");
+        return false;
+    }
+}
+
+//email  validator
+function validdateMail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        displayError("#email_error", "none");
+        return true;
+    } else {
+
+        displayError("#email_error", "block");
+        return false;
+    }
+}
+
+
+function displayError(elementId, elementProperty) {
+    return form.querySelector(elementId).style.display = elementProperty;
+}
