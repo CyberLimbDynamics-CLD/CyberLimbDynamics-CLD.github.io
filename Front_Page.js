92a9f61128bf80a945cb4fb80f14ae3a5710d0c0
Eric Albert <eric.albert8787@gmail.com>
2023-11-07 18:31:58 -0500

    Adding JS to Front_Page for the Survey

---
+++ Front_Page.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function()  {
+    var Take_Our_Survey = document.getElementById("Take_Our_Survey");
+
+    if (Take_Our_Survey) {
+        Take_Our_Survey.addEventListener("click", function() {
+            window.location.href = ''
+        });
+    }
+});
